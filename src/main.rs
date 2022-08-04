499f3231fd03903592f52c82f2227e1bcb0b38fa
Andrew Myers <andrewshannonmyers@gmail.com>
2022-08-03 22:14:11 -0500

    Test the find_matches function

--- src/main.rs
+++ src/main.rs
@@ -37,7 +37,7 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-fn find_matches(file_buffer: BufReader<File>, pattern: &str, mut writer: impl std::io::Write) -> Result<()>{
+fn find_matches<T: std::io::Read>(file_buffer: BufReader<T>, pattern: &str, mut writer: impl std::io::Write) -> Result<()>{
     let mut line_num = 1;
 
     for line in file_buffer.lines() {
@@ -50,4 +50,20 @@ fn find_matches(file_buffer: BufReader<File>, pattern: &str, mut writer: impl st
     }
 
     Ok(())
+}
+
+#[test]
+fn find_a_match() {
+    use std::io::{Cursor, Seek, SeekFrom, Write};
+
+    let mut result = Vec::new();
+
+    let test_string = b"lorem ipsum\ndolor sit amet\nipsum lorem";
+
+    let mut cursor = Cursor::new(Vec::new());
+    cursor.write(test_string).unwrap();
+    cursor.seek(SeekFrom::Start(0)).unwrap();
+
+    find_matches(BufReader::new(cursor.clone()), "lorem", &mut result).unwrap();
+    assert_eq!(result, b"1: lorem ipsum\n3: ipsum lorem\n");
 }
